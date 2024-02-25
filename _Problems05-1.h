18dc49d1614bbff52d8c1510f947b5b9a0cb5d43
Leonid Sokolinsky <Leonid.Sokolinsky@susu.ru>
2024-02-25 13:38:29 +0500

    Add files via upload
    Final edition for dimension 5

---
+++ _Problems05-1.h
@@ -0,0 +1,56 @@
+/*==============================================================================
+Project: LiFe
+Theme: Quest (No MPI)
+Module: Problems05-1.h (LP problems of dimension 5 with 1 randome inequality)
+Prefix: PP
+Author: Leonid B. Sokolinsky
+This include file is part of Problem-Parameters.h
+LP problems were obtained using BSF-LPP-Generator.
+==============================================================================*/
+#pragma once
+
+//=========================== Method Parameters =========================
+#define PP_EPS_ZERO		1E-9	// Accuracy for comparison with zero
+#define PP_ETA_TO_APEX	1000000	// Distance from apex base to apex point
+
+/*============================== rnd5-0 LP problem ==============================*
+#define PP_PROBLEM_NAME	"rnd5-0"
+#define PP_M 6		// Number of equations (number of rows in *.mtx)
+#define PP_N 11		// Number of variables (number of cols in *.mtx)
+//------------------------------------------------------------------/**/
+
+/*============================== rnd5-1-1 LP problem ==============================*
+#define PP_PROBLEM_NAME	"rnd5-1-1"
+#define PP_M 6		// Number of equations (number of rows in *.mtx)
+#define PP_N 11		// Number of variables (number of cols in *.mtx)
+//------------------------------------------------------------------/**/
+
+/*============================== rnd5-1-2 LP problem ==============================*/
+#define PP_PROBLEM_NAME	"rnd5-1-2"
+#define PP_M 6		// Number of equations (number of rows in *.mtx)
+#define PP_N 11		// Number of variables (number of cols in *.mtx)
+//------------------------------------------------------------------/**/
+
+/*============================== rnd5-1-3 LP problem ==============================*
+#define PP_PROBLEM_NAME	"rnd5-1-3"
+#define PP_M 6		// Number of equations (number of rows in *.mtx)
+#define PP_N 11		// Number of variables (number of cols in *.mtx)
+//------------------------------------------------------------------/**/
+
+/*============================== rnd5-1-4 LP problem ==============================*
+#define PP_PROBLEM_NAME	"rnd5-1-4"
+#define PP_M 6		// Number of equations (number of rows in *.mtx)
+#define PP_N 11		// Number of variables (number of cols in *.mtx)
+//------------------------------------------------------------------/**/
+
+/*============================== rnd5-1-5 LP problem ==============================*
+#define PP_PROBLEM_NAME	"rnd5-1-5"
+#define PP_M 6		// Number of equations (number of rows in *.mtx)
+#define PP_N 11		// Number of variables (number of cols in *.mtx)
+//------------------------------------------------------------------/**/
+
+/*============================== rnd5-1-6 LP problem ==============================*
+#define PP_PROBLEM_NAME	"rnd5-1-6"
+#define PP_M 6		// Number of equations (number of rows in *.mtx)
+#define PP_N 11		// Number of variables (number of cols in *.mtx)
+//------------------------------------------------------------------/**/
